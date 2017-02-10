e58880c583c2149ea3fe3c75750a75cbf09a51a5
SuperLinguini <milind.lingineni@gmail.com>
2017-02-09 21:25:51 -0500

    Started Login and Registration activities

--- app/src/main/java/tech/milind/cleanwatercrowdsourcing/MainActivity.java
+++ app/src/main/java/tech/milind/cleanwatercrowdsourcing/MainActivity.java
@@ -1,7 +1,10 @@
 package tech.milind.cleanwatercrowdsourcing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login = (Button) findViewById(R.id.buttonLogin);
+        Button register = (Button) findViewById(R.id.buttonRegister);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(i);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, LoginAcitivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
